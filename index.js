3b8f0c150bd324ef84bc13f137b3dd8741dedcb8
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 11:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,8 @@ function theBeatlesPlay(musicians, instruments) {
   return phrases;
 }
 
-function johnLennonFacts(){
-  var facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ];
+function johnLennonFacts(facts){
+  
   var newFacts = [];
   var i = 0
   while(i < facts.length){
