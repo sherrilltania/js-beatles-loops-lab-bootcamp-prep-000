c5b73baccb3e340611f88ad528ef49e6f79586f8
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 11:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var arr = []
-  for(let i=0; i<musicians.length; i++){
-    arr.push(musicians[1] + 'plays' + instruments[i])
+  for(let i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + 'plays' + instruments[i])
   }
   return arr
 }
