0497b31bb9038bbb5553285a39cf20b84243ae7d
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 11:08:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var arr = []
+  var arr = [];
   for(var i = 0; i < musicians.length; i++){
-    arr.push(musicians[i] +"plays" + instruments[i])
+    arr.push(musicians[i] +"plays" + instruments[i]);
   }
-  return arr
+  return arr;
 }
